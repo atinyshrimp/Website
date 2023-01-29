19a928ade88915179ea64ef49feb20bb9ea9b7a0
Joyce <99140907+atinyzen@users.noreply.github.com>
2023-01-29 17:55:07 +0100

    changed images & updated padding between sections

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -5,8 +5,7 @@ export const Footer = () => {
                 <div>Template by <a 
                 href="https://github.com/judygab/web-dev-projects/blob/main/personal-portfolio"
                 target="_blank">judygab</a></div>
-                <div>Customization built by Joyce Lapilus</div>
-                <div>&copy;</div>
+                <div>Customization built by Joyce Lapilus  &copy;</div>
             </div>
         </section>
     )
