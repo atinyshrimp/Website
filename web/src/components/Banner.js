52719b974263362d617a124f6a64448ab52d9d69
Joyce <99140907+atinyzen@users.noreply.github.com>
2023-02-08 02:20:39 +0100

    added picture
    added memoji representing me (might change it later)

--- web/src/components/Banner.js
+++ web/src/components/Banner.js
@@ -3,6 +3,7 @@ import { Col, Container, Row } from "react-bootstrap"
 import { Download } from "react-bootstrap-icons";
 import headerImg from "../assets/img/Totoro.png";
 import { CromerHourglass } from "./CromerHourglass";
+import profilepic from "../assets/img/profile_pic.png";
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
@@ -46,10 +47,11 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">she/her | INFJ-A</span>
-                        <h1>{"Hi I'm Joyce ! "}
-                        {/* <span>{text}</span> */}
-                        </h1>
+                        <div className="personal-info">
+                            <img src={profilepic} id="profile"/>
+                            <span className="tagline">she/her | INFJ-A</span>
+                        </div>
+                        <h1>{"Hi I'm Joyce ! "}</h1>
                         <p>I am a 20 year old engineering student located in France. My interests are video games, volleyball, playing music, and learning about languages & cultures</p>
                         <a href="https://www.canva.com/design/DAFZcVFIdso/SujFrqK9LDfwA3yQlcS5NQ/view?utm_content=DAFZcVFIdso&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
                         target="_blank">Get My Resume !<div class="download-btn"><Download size={25}/></div></a>
