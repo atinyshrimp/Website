1d29aac5ec6976ac33507a215eb668fab213545e
Joyce <99140907+atinyzen@users.noreply.github.com>
2023-10-31 14:09:37 +0100

    Update Banner.js
    Changed resume's link

--- web/src/components/Banner.js
+++ web/src/components/Banner.js
@@ -16,7 +16,7 @@ export const Banner = () => {
                         </div>
                         <h1>{"Hi I'm Joyce ! "}</h1>
                         <p>I am a 20 year old engineering student located in France. My interests are video games, volleyball, playing music, and learning about languages & cultures !</p>
-                        <a href="https://www.canva.com/design/DAFZcVFIdso/SujFrqK9LDfwA3yQlcS5NQ/view?utm_content=DAFZcVFIdso&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+                        <a href="https://www.canva.com/design/DAFtgK4TQTA/37acIWg-fDQpgsgUcHuwIQ/view?utm_content=DAFtgK4TQTA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
                         target="_blank">Get My Resume !<div class="download-btn"><Download size={25}/></div></a>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
