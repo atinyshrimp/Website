fa4c7dac0273fb1d0bef3676232e943b8721c3f7
Joyce <lianajoycemarie@gmail.com>
2024-08-12 23:41:30 +0200

    Added lastest pro experience

--- web/src/components/ExperienceList.js
+++ web/src/components/ExperienceList.js
@@ -1,21 +1,20 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { Box, Tab, Tabs, Typography } from '@mui/material';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
 
 const isHorizontal = window.innerWidth < 600;
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
-  if (!isHorizontal)
-  {
+  if (!isHorizontal) {
     return (
       <div
         role="tabpanel"
         hidden={value !== index}
         id={`vertical-tabpanel-${index}`}
         aria-labelledby={`vertical-tab-${index}`}
-        display='flex'
+        display="flex"
         {...other}
       >
         {value === index && (
@@ -28,19 +27,19 @@ function TabPanel(props) {
   } else {
     return (
       <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-with-tab-${index}`}
-      display='flex'
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-with-tab-${index}`}
+        display="flex"
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
     );
   }
 }
@@ -52,22 +51,32 @@ TabPanel.propTypes = {
 };
 
 function a11yProps(index) {
-  if (!isHorizontal)
-  {
+  if (!isHorizontal) {
     return {
       id: `vertical-tab-${index}`,
-      'aria-controls': `vertical-tabpanel-${index}`,
+      "aria-controls": `vertical-tabpanel-${index}`,
     };
   } else {
     return {
       id: `full-width-tab-${index}`,
-      "aria-controls": `full-width-tabpanel-${index}`
+      "aria-controls": `full-width-tabpanel-${index}`,
     };
   }
 }
 
 const professionalExp = {
   /* most recent to oldest */
+  Stellantis: {
+    title: "Data Analyst Junior Connected Vehicle @",
+    link: "https://www.stellantis.com/fr",
+    duration: "APR 2024 - AUG 2024",
+    desc: [
+      "Developed an interactive map using Power BI to quickly locate vehicles with connectivity issues, facilitating the identification of areas requiring intervention.",
+      "Created a system to classify and analyze delays in vehicle data communication, identifying causes by region and proposing tailored solutions.",
+      "Implemented an automated process for daily updates of vehicle data, ensuring that information is always current and reliable for analysis.",
+      "Automated data collection using Python and created interactive visual reports in Power BI, enabling real-time performance monitoring and a swift response to detected issues.",
+    ],
+  },
   "SNCF Réseau": {
     title: "Research Engineer Intern @",
     link: "https://www.sncf-reseau.com/fr",
@@ -77,8 +86,8 @@ const professionalExp = {
       "Identified canceled standards and researched alternative standards",
       "Specified test types and sanctions for technical validation by type of conductor",
       "Synthesized (e.g.: Excel table) the standards and the technical validation tests by type of conductor",
-      "Mapped out the gathered information in a Word document shared and used by the whole research section"
-    ]
+      "Mapped out the gathered information in a Word document shared and used by the whole research section",
+    ],
   },
 
   "Avenir APEI": {
@@ -89,8 +98,8 @@ const professionalExp = {
       "70-hour volunteer work",
       "Acted as a holiday camp activity leader to mentally handicapped children during Easter holidays",
       "Welcomed the children and their parents in the morning & sent them away in the evening",
-      "Supervised the different activities carried out during the day"
-    ]
+      "Supervised the different activities carried out during the day",
+    ],
   },
 
   "Visages du Monde": {
@@ -101,10 +110,9 @@ const professionalExp = {
       "Greeted visitors of the media library",
       "Informed visitors about the operation of the media library",
       "Put away the medias in their respective storage",
-      "Redacted an article for the media library's website"
-    ]
-  }
-  
+      "Redacted an article for the media library's website",
+    ],
+  },
 };
 
 const educationExp = {
@@ -117,10 +125,10 @@ const educationExp = {
       "Following a continuous Soft Skills formation, including agile methods like Scrum, teamwork and MBTI.",
       "Yearly projects in accordance with the modules seen throughout the year",
       "Enrolled in the UX Design track including courses such as GUI prototyping, Website Design, Game Design, etc.",
-      "Completing a double degree with the Management school, EMLV (e.g. tab under 'ESILV')"
-    ]
+      "Completing a double degree with the Management school, EMLV (e.g. tab under 'ESILV')",
+    ],
   },
-  
+
   EMLV: {
     /* International Business, Digital HR, Corporate Finance or Finance & Investment */
     title: "MSc in Finance @",
@@ -128,8 +136,8 @@ const educationExp = {
     duration: "SEPT 2020 - NOW",
     desc: [
       "Followed courses in Accounting, Fundamental Marketing, Corporate Strategy, Supply Chain Management, Entrepreneurship and International & European Law",
-      "Following courses in Cash Management and Financial Decisions"
-    ]
+      "Following courses in Cash Management and Financial Decisions",
+    ],
   },
 
   CAU: {
@@ -138,8 +146,8 @@ const educationExp = {
     duration: "SEPT 2022 - DEC 2022",
     desc: [
       "Studied on the Seoul campus of Chung-Ang University",
-      "Followed courses in Object-Oriented Programming (C++), Web Programming, Numerical Analysis & Intermediate Korean Language"
-    ]
+      "Followed courses in Object-Oriented Programming (C++), Web Programming, Numerical Analysis & Intermediate Korean Language",
+    ],
   },
 
   "Lycée Jules Verne": {
@@ -150,14 +158,12 @@ const educationExp = {
       "Obtained with distinctions",
       "Class representative in 11th & 12th year",
       "Baccaulauréat S-SVT (Science stream, biology focus)",
-      "Chose Computer Science & Digital Sciences (ISN) specialty in 12th grade"
-    ]
-  }
+      "Chose Computer Science & Digital Sciences (ISN) specialty in 12th grade",
+    ],
+  },
 };
 
-
 export const ProfessionalList = () => {
-  
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
@@ -173,38 +179,40 @@ export const ProfessionalList = () => {
         variant={isHorizontal ? "fullWidth" : "scrollable"}
         value={value}
         onChange={handleChange}
-        sx={{ borderRight: 1, borderColor: 'divider', float: 'left', '& .MuiTabs-indicator': {backgroundColor: '#A499BE'} }}
+        sx={{
+          borderRight: 1,
+          borderColor: "divider",
+          float: "left",
+          "& .MuiTabs-indicator": { backgroundColor: "#A499BE" },
+        }}
       >
-          {Object.keys(list).map((key, i) => (
-              <Tab label={isHorizontal ? `0${i}.` : key} {...a11yProps(i)} />
-          ))}
+        {Object.keys(list).map((key, i) => (
+          <Tab label={isHorizontal ? `0${i}.` : key} {...a11yProps(i)} />
+        ))}
       </Tabs>
       {Object.keys(list).map((key, i) => (
-          <TabPanel value={value} index={i}>
-              <span className='list-title'>
-                  {list[key]["title"] + " "}
-              </span>
-              <span className='list-location'><a href={list[key]["link"]} target="_blank">{key}</a></span>
-              <div className='list-duration'>
-                  {list[key]["duration"]}
-              </div>
-              <ul className='description'>
-                  {list[key]["desc"].map(function (descItem, i) {
-                      return (
-                        <li key={i}>{descItem}</li>
-                      );
-                  })}
-              </ul>
-          </TabPanel>
+        <TabPanel value={value} index={i}>
+          <span className="list-title">{list[key]["title"] + " "}</span>
+          <span className="list-location">
+            <a href={list[key]["link"]} target="_blank">
+              {key}
+            </a>
+          </span>
+          <div className="list-duration">{list[key]["duration"]}</div>
+          <ul className="description">
+            {list[key]["desc"].map(function (descItem, i) {
+              return <li key={i}>{descItem}</li>;
+            })}
+          </ul>
+        </TabPanel>
       ))}
     </Box>
   );
-  
-}
+};
 
-export const EducationList = () => {  
+export const EducationList = () => {
   const [value, setValue] = React.useState(0);
-  
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -218,31 +226,33 @@ export const EducationList = () => {
         variant={isHorizontal ? "fullWidth" : "scrollable"}
         value={value}
         onChange={handleChange}
-        sx={{ borderRight: 1, borderColor: 'divider', float: 'left', '& .MuiTabs-indicator': {backgroundColor: '#A499BE'} }}
+        sx={{
+          borderRight: 1,
+          borderColor: "divider",
+          float: "left",
+          "& .MuiTabs-indicator": { backgroundColor: "#A499BE" },
+        }}
       >
-          {Object.keys(list).map((key, i) => (
-              <Tab label={isHorizontal ? `0${i}.` : key} {...a11yProps(i)} />
-              ))}
+        {Object.keys(list).map((key, i) => (
+          <Tab label={isHorizontal ? `0${i}.` : key} {...a11yProps(i)} />
+        ))}
       </Tabs>
       {Object.keys(list).map((key, i) => (
-          <TabPanel value={value} index={i}>
-              <span className='list-title'>
-                  {list[key]["title"] + " "}
-              </span>
-              <span className='list-location'><a href={list[key]["link"]} target="_blank">{key}</a></span>
-              <div className='list-duration'>
-                  {list[key]["duration"]}
-              </div>
-              <ul className='description'>
-                  {list[key]["desc"].map(function (descItem, i) {
-                      return (
-                        <li key={i}>{descItem}</li>
-                      );
-                  })}
-              </ul>
-          </TabPanel>
+        <TabPanel value={value} index={i}>
+          <span className="list-title">{list[key]["title"] + " "}</span>
+          <span className="list-location">
+            <a href={list[key]["link"]} target="_blank">
+              {key}
+            </a>
+          </span>
+          <div className="list-duration">{list[key]["duration"]}</div>
+          <ul className="description">
+            {list[key]["desc"].map(function (descItem, i) {
+              return <li key={i}>{descItem}</li>;
+            })}
+          </ul>
+        </TabPanel>
       ))}
     </div>
   );
-  
-}
+};
--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,12 +1,18 @@
 export const Footer = () => {
-    return (
-        <section className="footer">
-            <div id="credits">
-                <div>Template by <a 
-                href="https://github.com/judygab/web-dev-projects/blob/main/personal-portfolio"
-                target="_blank">judygab</a></div>
-                <div>Customization built by Joyce Lapilus  &copy;</div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="footer">
+      <div id="credits">
+        <div>
+          Template by{" "}
+          <a
+            href="https://github.com/judygab/web-dev-projects/blob/main/personal-portfolio"
+            target="_blank"
+          >
+            judygab
+          </a>
+        </div>
+        <div>Customization built by Joyce Lapilus &copy;</div>
+      </div>
+    </section>
+  );
+};
